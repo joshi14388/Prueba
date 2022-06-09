eb9621afabaefda826d89220147828e7d719aa45
Jose Garcia <joshi14388@gmail.com>
2022-06-09 18:58:51 -0400

    interceptors y auth guards

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,19 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+// !codigo original del proyecto
+/* export const environment = {
+  production: false
+}; */
+
+//! codigo prueba
 export const environment = {
+  API_PROTOCOL: 'http',
+  API_HOST: 'localhost',
+  API_PORT: 8080,
+  API_VERSION: 1,
+  //MAP_API_KEY: 'AIzaSyBrfUxW92sWif6epUCe2Bbz15j-xnI-CUc',
   production: false
 };
 
